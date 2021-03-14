db89fbea471445c68a10ee843c9a442976f5c879
Billy Wu <bobpost515@gmail.com>
2021-03-14 00:28:36 -0500

    test

---
+++ card-game/src/components/deck/deck.js
@@ -0,0 +1,369 @@
+/* eslint-disable prettier/prettier */
+const deck = [{
+        code: "AS",
+        image: "../deck/images/AS",
+        value: "ACE",
+        blackjack: 1 || 11,
+        suit: "SPADES"
+    },
+    {
+        code: "2S",
+        image: "../deck/images/",
+        value: "2",
+        blackjack: 2,
+        suit: "SPADES"
+    },
+    {
+        code: "3S",
+        image: "../deck/images/",
+        value: "33",
+        blackjack: 3,
+        suit: "SPADES"
+    },
+    {
+        code: "4S",
+        image: "../deck/images/",
+        value: "44",
+        blackjack: 4,
+        suit: "SPADES"
+    },
+    {
+        code: "5S",
+        image: "../deck/images/",
+        value: "55",
+        blackjack: 5,
+        suit: "SPADES"
+    },
+    {
+        code: "6S",
+        image: "../deck/images/",
+        value: "66",
+        blackjack: 6,
+        suit: "SPADES"
+    },
+    {
+        code: "7S",
+        image: "../deck/images/",
+        value: "77",
+        blackjack: 7,
+        suit: "SPADES"
+    },
+    {
+        code: "8S",
+        image: "../deck/images/",
+        value: "88",
+        blackjack: 8,
+        suit: "SPADES"
+    },
+    {
+        code: "9S",
+        image: "../deck/images/",
+        value: "99",
+        blackjack: 9,
+        suit: "SPADES"
+    },
+    {
+        code: "0S",
+        image: "../deck/images/",
+        value: "1010",
+        blackjack: 10,
+        suit: "SPADES"
+    },
+    {
+        code: "JS",
+        image: "../deck/images/",
+        value: "JACKJACK",
+        blackjack: 10,
+        suit: "SPADES"
+    },
+    {
+        code: "QS",
+        image: "../deck/images/",
+        value: "JAQUEENCK",
+        blackjack: 10,
+        suit: "SPADES"
+    },
+    {
+        code: "KS",
+        image: "../deck/images/",
+        value: "JACKKING",
+        blackjack: 10,
+        suit: "SPADES"
+    },
+    {
+        code: "AD",
+        image: "../deck/images/",
+        value: "ACEACE",
+        blackjack: 1 || 11,
+        suit: "DIAMONDS"
+    },
+    {
+        code: "2D",
+        image: "../deck/images/",
+        value: "22",
+        blackjack: 2,
+        suit: "DIAMONDS"
+    },
+    {
+        code: "3D",
+        image: "../deck/images/",
+        value: "3",
+        blackjack: 3,
+        suit: "DIAMONDS"
+    },
+    {
+        code: "4D",
+        image: "../deck/images/",
+        value: "4",
+        blackjack: 4,
+        suit: "DIAMONDS"
+    },
+    {
+        code: "5D",
+        image: "../deck/images/",
+        value: "5",
+        blackjack: 5,
+        suit: "DIAMONDS"
+    },
+    {
+        code: "6D",
+        image: "../deck/images/",
+        value: "6",
+        blackjack: 6,
+        suit: "DIAMONDS"
+    },
+    {
+        code: "7D",
+        image: "../deck/images/",
+        value: "7",
+        blackjack: 7,
+        suit: "DIAMONDS"
+    },
+    {
+        code: "8D",
+        image: "../deck/images/",
+        value: "8",
+        blackjack: 8,
+        suit: "DIAMONDS"
+    },
+    {
+        code: "9D",
+        image: "../deck/images/",
+        value: "9",
+        blackjack: 9,
+        suit: "DIAMONDS"
+    },
+    {
+        code: "0D",
+        image: "../deck/images/",
+        value: "10",
+        blackjack: 10,
+        suit: "DIAMONDS"
+    },
+    {
+        code: "JD",
+        image: "../deck/images/",
+        value: "JACK",
+        blackjack: 10,
+        suit: "DIAMONDS"
+    },
+    {
+        code: "QD",
+        image: "../deck/images/",
+        value: "QUEEN",
+        blackjack: 10,
+        suit: "DIAMONDS"
+    },
+    {
+        code: "KD",
+        image: "../deck/images/",
+        value: "KING",
+        blackjack: 10,
+        suit: "DIAMONDS"
+    },
+    {
+        code: "AC",
+        image: "../deck/images/",
+        value: "ACE",
+        blackjack: 1 || 11,
+        suit: "CLUBS"
+    },
+    {
+        code: "2C",
+        image: "../deck/images/",
+        value: "2",
+        blackjack: 2,
+        suit: "CLUBS"
+    },
+    {
+        code: "3C",
+        image: "../deck/images/",
+        value: "3",
+        blackjack: 3,
+        suit: "CLUBS"
+    },
+    {
+        code: "4C",
+        image: "../deck/images/",
+        value: "4",
+        blackjack: 4,
+        suit: "CLUBS"
+    },
+    {
+        code: "5C",
+        image: "../deck/images/",
+        value: "5",
+        blackjack: 5,
+        suit: "CLUBS"
+    },
+    {
+        code: "6C",
+        image: "../deck/images/",
+        value: "6",
+        blackjack: 6,
+        suit: "CLUBS"
+    },
+    {
+        code: "7C",
+        image: "../deck/images/",
+        value: "7",
+        blackjack: 7,
+        suit: "CLUBS"
+    },
+    {
+        code: "8C",
+        image: "../deck/images/",
+        value: "8",
+        blackjack: 8,
+        suit: "CLUBS"
+    },
+    {
+        code: "9C",
+        image: "../deck/images/",
+        value: "9",
+        blackjack: 9,
+        suit: "CLUBS"
+    },
+    {
+        code: "0C",
+        image: "../deck/images/",
+        value: "10",
+        blackjack: 10,
+        suit: "CLUBS"
+    },
+    {
+        code: "JC",
+        image: "../deck/images/",
+        value: "JACK",
+        blackjack: 10,
+        suit: "CLUBS"
+    },
+    {
+        code: "QC",
+        image: "../deck/images/",
+        value: "QUEEN",
+        blackjack: 10,
+        suit: "CLUBS"
+    },
+    {
+        code: "KC",
+        image: "../deck/images/",
+        value: "KING",
+        blackjack: 10,
+        suit: "CLUBS"
+    },
+    {
+        code: "AH",
+        image: "../deck/images/",
+        value: "ACE",
+        blackjack: 1 || 11,
+        suit: "HEARTS"
+    },
+    {
+        code: "2H",
+        image: "../deck/images/",
+        value: "2",
+        blackjack: 2,
+        suit: "HEARTS"
+    },
+    {
+        code: "3H",
+        image: "../deck/images/",
+        value: "3",
+        blackjack: 3,
+        suit: "HEARTS"
+    },
+    {
+        code: "4H",
+        image: "../deck/images/",
+        value: "4",
+        blackjack: 4,
+        suit: "HEARTS"
+    },
+    {
+        code: "5H",
+        image: "../deck/images/",
+        value: "5",
+        blackjack: 5,
+        suit: "HEARTS"
+    },
+    {
+        code: "6H",
+        image: "../deck/images/",
+        value: "6",
+        blackjack: 6,
+        suit: "HEARTS"
+    },
+    {
+        code: "7H",
+        image: "../deck/images/",
+        value: "7",
+        blackjack: 7,
+        suit: "HEARTS"
+    },
+    {
+        code: "8H",
+        image: "../deck/images/",
+        value: "8",
+        blackjack: 8,
+        suit: "HEARTS"
+    },
+    {
+        code: "9H",
+        image: "../deck/images/",
+        value: "9",
+        blackjack: 9,
+        suit: "HEARTS"
+    },
+    {
+        code: "0H",
+        image: "../deck/images/",
+        value: "10",
+        blackjack: 10,
+        suit: "HEARTS"
+    },
+    {
+        code: "JH",
+        image: "../deck/images/",
+        value: "JACK",
+        blackjack: 10,
+        suit: "HEARTS"
+    },
+    {
+        code: "QH",
+        image: "../deck/images/",
+        value: "QUEEN",
+        blackjack: 10,
+        suit: "HEARTS"
+    },
+    {
+        code: "KH",
+        image: "../deck/images/",
+        value: "KING",
+        blackjack: 10,
+        suit: "HEARTS"
+    }
+];
+export {
+    deck
+};
