6ac9838d0a08506b2e02482bd004fbf7f0b945fe
TulloSITHS <50631839+TulloSITHS@users.noreply.github.com>
2021-03-23 22:43:18 -0400

    a transition to create/join game added
    just generally added some more stuff to the main menu

--- card-game/src/views/home.vue
+++ card-game/src/views/home.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="container">
-    <button @click="playGame()">Play</button>
+    <button v-on:click="play = !play">Play</button>
     <button>About</button>
     <button>Credits</button>
     <button>Donate</button>
     <button @click="auth()">Log In | Sign Up</button>
     <p>The Coolest Game Card</p>
+        <transition name="fade">
+            <div class="player" v-if="play">
+                <button>Create Game</button>
+                <button @click="playGame()">Join Game</button>
+            </div>
+        </transition>
   </div>
 </template>
 
 <script>
 export default {
   data() {
-
+    return {
+        play: false
+    }
   },
   methods: {
     playGame: function() {
@@ -24,3 +32,11 @@ export default {
     }
 }
 </script>
+<style>
+    .fade-enter-active, .fade-leave-active {
+    transition: opacity .5s;
+    }
+    .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+    opacity: 0;
+    }
+</style>
